95a302d75bebfa7c3a36913624666461ea571c60
IngTony <ingmoisestoro@gmail.com>
2025-09-15 13:05:24 -0600

    fix: :bug: fix bugs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
 
-import { AuthRoutes } from './AuthRoutes'
 import { UserRoutes } from './UserRoutes'
 
-import { AppRouter } from '@/components/AppRouter'
+import AppRouter from '@/components/AppRouter'
+import LoginPage from '@/pages/login'
+import RegisterPage from '@/pages/register'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<Navigate replace to="/login" />} path="/" />
-      <Route element={<AuthRoutes />} path="/auth/*" />
+      <Route element={<LoginPage />} path="/login" />
+      <Route element={<RegisterPage />} path="/register" />
       <Route
         element={
           <AppRouter>
